233d01d1a1e69d403de4cfbd517db27fb0979f91
Vladimir Petrushenka <volodia320@gmail.com>
2020-03-05 18:00:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# final_task_petrushenko_autumn2020
+ My final task
---
+++ src/main/java/by/simplypvs/servlets/listeners/AppContextListener.java
@@ -0,0 +1,18 @@
+package by.simplypvs.servlets.listeners;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class AppContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}
